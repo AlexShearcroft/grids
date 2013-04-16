940bf48c6f94b51cf5b1e3ad24e2bd5dbd86a166
Alex Shearcroft <alex@stormcreative.co.uk>
2013-04-16 14:53:33 +0100

    first version of grid system

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<!--[if IE 8]>
+<html dir="ltr" lang="en" class="ie8">
+<![endif]-->
+<!--[if IE 9]>
+<html dir="ltr" lang="en" class="ie9">
+<![endif]-->
+<!--[if gt IE 9]>
+<html dir="ltr" lang="en">
+<![endif]-->
+	<head>
+		<title>Grid</title>
+		<meta charset="utf-8">
+        <script src="Modernizr.js"></script>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0'/>
+		<link href='layout.css' rel='stylesheet'/>
+	</head>
+	<body>
+		<section class="grid">
+			<div class="grid__layout"><p>Grid</p></div>
+
+			<div class="grid__layout grid__layout--one-half"><p>one-half</p></div>
+			<div class="grid__layout grid__layout--one-half"><p>one-half</p></div>
+			
+			<div class="grid__layout grid__layout--one-third"><p>one-third</p></div>
+			<div class="grid__layout grid__layout--one-third"><p>one-third</p></div>
+			<div class="grid__layout grid__layout--one-third"><p>one-third</p></div>
+
+			<div class="grid__layout grid__layout--one-quarter"><p>one-quarter</p></div>
+			<div class="grid__layout grid__layout--one-quarter"><p>one-quarter</p></div>
+			<div class="grid__layout grid__layout--one-quarter"><p>one-quarter</p></div>
+			<div class="grid__layout grid__layout--one-quarter"><p>one-quarter</p></div>
+
+			<div class="grid__layout grid__layout--one-third"><p>one-third</p></div>
+			<div class="grid__layout grid__layout--two-thirds"><p>two-thirds</p></div>
+
+			<div class="grid__layout grid__layout--one-half"><p>one-half</p></div>
+			<div class="grid__layout grid__layout--one-quarter"><p>one-quarter</p></div>
+			<div class="grid__layout grid__layout--one-quarter"><p>one-quarter</p></div>
+		
+			<div class="grid__layout grid__layout--one-quarter"><p>one-quarter</p></div>
+			<div class="grid__layout grid__layout--three-quarters"><p>three-quarters</p></div>
+
+			<div class="grid__layout grid__layout--one-sixth"><p>one-sixth</p></div>
+			<div class="grid__layout grid__layout--one-sixth"><p>one-sixth</p></div>
+			<div class="grid__layout grid__layout--one-sixth"><p>one-sixth</p></div>
+			<div class="grid__layout grid__layout--one-sixth"><p>one-sixth</p></div>
+			<div class="grid__layout grid__layout--one-sixth"><p>one-sixth</p></div>
+			<div class="grid__layout grid__layout--one-sixth"><p>one-sixth</p></div>
+		</section>
+	</body>
+</html>
